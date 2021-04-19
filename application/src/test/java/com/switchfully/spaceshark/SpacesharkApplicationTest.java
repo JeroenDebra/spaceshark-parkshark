9df846083b27b5429d6cdfceb9b0fe6239bfc946
Jeroen Debra <jeroen.debra@minfin.fed.be>
2021-04-19 14:49:53 +0200

    hello world

---
+++ application/src/test/java/com/switchfully/spaceshark/SpacesharkApplicationTest.java
@@ -0,0 +1,13 @@
+package com.switchfully.spaceshark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpacesharkApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
