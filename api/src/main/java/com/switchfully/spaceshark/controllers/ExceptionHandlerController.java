3874a07353312548776c4da8aa10c0dd0140eafb
Jeroen Debra <jeroen.debra@minfin.fed.be>
2021-04-23 11:18:47 +0200

    STORY ExceptionHandlerController | added handlers for more exceptions

--- api/src/main/java/com/switchfully/spaceshark/controllers/ExceptionHandlerController.java
+++ api/src/main/java/com/switchfully/spaceshark/controllers/ExceptionHandlerController.java
@@ -1,5 +1,9 @@
 package com.switchfully.spaceshark.controllers;
 
+import com.switchfully.spaceshark.customExeptions.CategoryNotValidException;
+import com.switchfully.spaceshark.customExeptions.CurrencyNotValidException;
+import com.switchfully.spaceshark.customExeptions.NotAuthorizedException;
+import com.switchfully.spaceshark.model.parkingLot.Category;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -19,4 +23,30 @@ public class ExceptionHandlerController extends ResponseEntityExceptionHandler {
         LOGGER.warn(exception.getMessage(), exception);
         response.sendError(HttpServletResponse.SC_BAD_REQUEST, exception.getMessage());
     }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public void handleIllegalState(IllegalStateException exception, HttpServletResponse response) throws IOException{
+        LOGGER.warn(exception.getMessage(), exception);
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST, exception.getMessage());
+    }
+
+    @ExceptionHandler(NotAuthorizedException.class)
+    public void handleNotAuthorized(NotAuthorizedException exception, HttpServletResponse response) throws IOException{
+        LOGGER.warn(exception.getMessage(), exception);
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, exception.getMessage());
+    }
+
+    @ExceptionHandler(CategoryNotValidException.class)
+    public void handleInvalidCategory(CategoryNotValidException exception, HttpServletResponse response) throws IOException{
+        LOGGER.warn(exception.getMessage(), exception);
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST, exception.getMessage());
+    }
+
+    @ExceptionHandler(CurrencyNotValidException.class)
+    public void handleInvalidcurrency(CurrencyNotValidException exception, HttpServletResponse response) throws IOException{
+        LOGGER.warn(exception.getMessage(), exception);
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST, exception.getMessage());
+    }
+
+
 }
